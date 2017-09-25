19e7b7fb9b399f6ca0386e37060835ba7988bf7a
Luis Rosales <eng.luisrosales@gmail.com>
2017-09-25 23:29:26 +0200

    limpiamos los ejemplos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,21 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-
-    // static cosasStatic = [100,'string', true]; // propiedad de la clase
-    // cosas = [100,'string', true]; // propiedad de la instancia
-    // cosas2: any[] = [100,'string', true];
-    // cosasStr: string[] = ['hola','string', 'wawa'];
-}
-
-class Coche {
-
-  constructor(public ruedas:number, public puertas: number) { }
-}
-
-let coche1 = new Coche(4, 3);
-coche1.ruedas;
-
-let coche2 = new Coche(5, 5);
-coche2.ruedas;
+export class AppComponent { }
