3268724d6dff6172b11a1beacac4c3c5c6b2b557
Luis Rosales <eng.luisrosales@gmail.com>
2017-09-25 23:28:26 +0200

    ejemplos de propiedades y constructor de clase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+    // static cosasStatic = [100,'string', true]; // propiedad de la clase
+    // cosas = [100,'string', true]; // propiedad de la instancia
+    // cosas2: any[] = [100,'string', true];
+    // cosasStr: string[] = ['hola','string', 'wawa'];
 }
+
+class Coche {
+
+  constructor(public ruedas:number, public puertas: number) { }
+}
+
+let coche1 = new Coche(4, 3);
+coche1.ruedas;
+
+let coche2 = new Coche(5, 5);
+coche2.ruedas;
