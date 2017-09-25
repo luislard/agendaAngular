17d411327882e113cac79306b71c46793a13f113
Luis Rosales <eng.luisrosales@gmail.com>
2017-09-26 00:00:34 +0200

    adding comments to module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// usamos el decorador 'NgModle' para que la clase decorada
+// se comporte como un modulo
 @NgModule({
+  // En el metadato 'declarations' indicamos todos aquellos
+  // componentes, pipes y directivas de mi aplicación.
   declarations: [
     AppComponent
   ],
+  // En el metadato 'imports' indicamos todos aquellos 
+  // módulos de los cuáles mi aplicación depende
   imports: [
     BrowserModule
   ],
+  //  en el metadato 'providers' indicamos todos aquellos 
+  // proveedores de clase o valores que puedan ser inyectados.
   providers: [],
-  bootstrap: [AppComponent]
+  // En el metadato 'bootstrap' indicamos el componente raíz
+  // a partir del cuál se construyr toda la aplicación.
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
