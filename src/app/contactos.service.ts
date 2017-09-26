4c035599bf9c55d661c7d5a7af257812f0bac7b2
Luis Rosales <eng.luisrosales@gmail.com>
2017-09-26 23:42:24 +0200

    agregamos el contactoService a los providers del app.module

--- src/app/contactos.service.ts
+++ src/app/contactos.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 
+// el decorador 'injectable' indica que la clase decorada 
+// debe comportarse como un servicio
 @Injectable()
 export class ContactosService {
 
