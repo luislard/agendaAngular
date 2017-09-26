91cbf5847e7f84a8d7638d39f578a796c34dac69
Luis Rosales <eng.luisrosales@gmail.com>
2017-09-26 23:59:07 +0200

    restructuramos el código para halar los datos desde un servicio

--- src/app/contactos.service.ts
+++ src/app/contactos.service.ts
@@ -5,6 +5,36 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ContactosService {
 
-  constructor() { }
+  // por convención si no ponemos private el atributo es public, 
+  // por convención si algo es privado colocamos el prefijo _<nombre>.
+  // se pone en privado para que nadie pueda modificarlos
+  private _nombres: string[] = [
+    'Bob Esponja',
+    'Elon Musk',
+    'Steve Jobs',
+    'Bill Gates',
+    'Sundar Picagai',
+    'Steve Wozniak',
 
+  ];
+
+  // getter de los contactos
+  obtenerContactos(): string[] {
+    return this._nombres;
+  }
+
+  eliminarContacto(nombre: string): void {
+    console.log(nombre);
+    
+    // Para eliminar un contacto lo que hacemos es
+    // filtrar la colección y quedarnos con todos
+    // aquellos que no sean el indicado
+
+    // la funcion filter itera por los elementos del array
+    // y se queda con los elementos que returnen true
+    // en este caso los nombres diferentes al nombre que
+    // hemos pulsado.
+    this._nombres = this._nombres.filter(n => n !== nombre);
+
+  }
 }
