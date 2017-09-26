e466fd32e2387b91abbd4f9ee138ff73b51aca1e
Luis Rosales <eng.luisrosales@gmail.com>
2017-09-26 21:19:57 +0200

    limpiamos el OnInit del import

--- src/app/lista-contactos/lista-contactos.component.ts
+++ src/app/lista-contactos/lista-contactos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-lista-contactos',
@@ -7,5 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaContactosComponent {
 
+  
 
 }
