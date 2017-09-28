8f8999ab9bc48dc2447285ae030ffded02d67fc9
Luis Rosales <eng.luisrosales@gmail.com>
2017-09-28 21:23:17 +0200

    volviendo a como estaba antes de los ejemplos de dependency injection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,14 +22,7 @@ import { ContactosService } from './contactos.service';
   //  en el metadato 'providers' indicamos todos aquellos 
   // proveedores de clase o valores que puedan ser inyectados.
   providers: [
-    // ContactosService
-    {
-      provide: ContactosService,
-      deps: [HttpClient], // hay que importarlo
-      useFactory: (http) => {
-        return new ContactosService(http);
-      }
-    }
+    ContactosService
   ],
   // En el metadato 'bootstrap' indicamos el componente raíz
   // a partir del cuál se construyr toda la aplicación.
